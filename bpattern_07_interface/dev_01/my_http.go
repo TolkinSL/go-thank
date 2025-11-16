451b447e1f3930fa0c6a1be5b5cda1aca7da4291
TolkinSL <tolkinsl@yandex.ru>
2025-11-16 17:59:06 +0300

    B02 Структуры

--- bpattern_07_interface/dev_01/my_http.go
+++ bpattern_07_interface/dev_01/my_http.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func LogOutput(message string) {
 	fmt.Println(message)
@@ -25,10 +28,62 @@ func NewSimpleDataStore() SimpleDataStore {
 	}
 }
 
+type DataStore interface {
+	UserNameForID(userID string) (string, bool)
+}
+
+type Logger interface {
+	Log(message string)
+}
+
+type LoggerAdapter func(message string)
+
+func (lg LoggerAdapter) Log(message string) {
+	lg(message)
+}
+
+// Бизнес Логика
+type SimpleLogic struct {
+	l Logger
+	ds DataStore
+}
+
+func (sl SimpleLogic) SayHello(userID string) (string, error) {
+	sl.l.Log(fmt.Sprintf("\nin method SayHello for ID - %s\n", userID))
+	name, ok := sl.ds.UserNameForID(userID)
+	if !ok {
+		return "", errors.New("unknown user")
+	}
+	return fmt.Sprintf("Hello - %s\n", name), nil
+}
+
 func main() {
-	my_struct := NewSimpleDataStore()
-	fmt.Printf("%+v\n", my_struct)
+	my_dataStruct := NewSimpleDataStore()
+	fmt.Printf("%+v\n", my_dataStruct)
 	
-	name, ok := my_struct.UserNameForID("7")
+	name, ok := my_dataStruct.UserNameForID("7")
 	fmt.Printf("%#v %#v\n", name, ok)
+
+	my_logger := LoggerAdapter(LogOutput)
+	my_logger.Log("Hello")
+
+	fmt.Println("-------------")
+
+	my_simpleStruct := SimpleLogic{
+		l: my_logger,
+		ds: my_dataStruct,
+	}
+
+	my_simpleStruct.l.Log("Test")
+
+	name, ok = my_simpleStruct.ds.UserNameForID("1")
+	fmt.Printf("%#v %#v\n", name, ok)
+
+	str, err := my_simpleStruct.SayHello("2")
+	fmt.Printf("%#v %#v\n", str, err)
+	fmt.Println(str)
+
+	str, err = my_simpleStruct.SayHello("7")
+	fmt.Printf("%#v %#v\n", str, err)
+	fmt.Println(str)
 }
