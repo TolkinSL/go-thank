061db2ffff24b090587195453b81fbcfd6d7709e
TolkinSL <tolkinsl@yandex.ru>
2025-11-16 15:16:32 +0300

    B01 Структуры

---
+++ bpattern_07_interface/dev_01/my_http.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func LogOutput(message string) {
+	fmt.Println(message)
+}
+
+type SimpleDataStore struct {
+	userData map[string]string
+}
+
+func (sds SimpleDataStore) UserNameForID(userID string) (string, bool) {
+	name, ok := sds.userData[userID]
+	return name, ok
+}
+
+func NewSimpleDataStore() SimpleDataStore {
+	return SimpleDataStore{
+		userData: map[string]string{
+			"1": "Fred",
+			"2": "Mary",
+			"3": "Pat",
+		},
+	}
+}
+
+func main() {
+	my_struct := NewSimpleDataStore()
+	fmt.Printf("%+v\n", my_struct)
+	
+	name, ok := my_struct.UserNameForID("7")
+	fmt.Printf("%#v %#v\n", name, ok)
+}
