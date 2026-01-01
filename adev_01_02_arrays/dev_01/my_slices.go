72719f356c5014b8bebaad4859b530aa99b19810
TolkinSL <tolkinsl@yandex.ru>
2026-01-01 22:12:18 +0300

    A02 Срезы

---
+++ adev_01_02_arrays/dev_01/my_slices.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	s1 := make([]string, 3)
+	fmt.Println(len(s1))
+	fmt.Println(cap(s1))
+	fmt.Printf("%#v\n", s1)
+
+	s2 := []string{"a", "b", "c"}
+	s2 = append(s2, "z")
+	fmt.Printf("%#v\n", s2)
+
+	dst := make([]string, len(s2))
+	copy(dst, s2)
+	fmt.Printf("%#v\n", s2)
+	fmt.Printf("%#v\n", s2[1:3])
+	fmt.Printf("%#v\n", s2[:2])
+	fmt.Printf("%#v\n", s2[2:])
+}
