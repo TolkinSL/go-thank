817b84cc8fc2243a3c5895265efcf497b614c271
TolkinSL <tolkinsl@yandex.ru>
2026-04-02 10:07:32 +0300

    B03 Context

--- bpattern_12_goroutines/dev_01/main.go
+++ bpattern_12_goroutines/dev_01/main.go
@@ -1,27 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
 
-func count() chan int {
+func count(ctx context.Context) chan int {
 	ch := make(chan int)
 	go func() {
 		defer close(ch)
-		for i := 0; i < 5; i++ {
-			ch <- i
+		for i := 0; i < 10; i++ {
+			select {
+			case ch <- i:
+			case <-ctx.Done():
+				fmt.Println("<-ctx.Done()")
+				return
+			}
 		}
-		
+
 	}()
 
 	return ch
 }
 
 func main() {
-	ch := count()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ch := count(ctx)
 
 	for val := range ch {
+		if val > 5 {
+			cancel()
+			break
+		}
 		fmt.Println(val)
 	}
 
